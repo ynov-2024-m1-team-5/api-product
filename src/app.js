ab85b7674eb629548cf49d7398060ec0ad5d7d27
okabasele <ocekabasele@gmail.com>
2024-02-08 12:01:04 +0100

    install cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 require("dotenv").config();
-
+const cors = require('cors');
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
